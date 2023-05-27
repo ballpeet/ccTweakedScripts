d71d1f577cd6d6a665c508370c4c6f2e11cb1607
ballpeet <94201310+ballpeet@users.noreply.github.com>
2023-05-27 19:45:43 -0400

    Update itemSorter.lua

--- itemSorter.lua
+++ itemSorter.lua
@@ -2,7 +2,7 @@ local inputChest = "minecraft:barrel_2"
 
 local junkChest = "minecraft:barrel_0"
 
--- MOST, LEAST
+-- STRICT, LEAST
 
 --NOT, IS, ALL
 --ID, TAG
@@ -15,12 +15,12 @@ local sortFilters = {
 
             {
                 ["value"] = {"minecraft:planks"},
-                ["type"] = "ID",
+                ["type"] = "TAG",
                 ["arg"] = "IS",
             },
 
         },
-        ["filterType"] = "TAG",
+        ["filterType"] = "STRICT",
         ["filterExtra"] = 1,
         ["priority"] = 0
     },
@@ -37,14 +37,14 @@ local sortFilters = {
             },
 
         },
-        ["filterType"] = "MOST",
+        ["filterType"] = "LEAST",
         ["filterExtra"] = 1,
         ["priority"] = 0
     },
 
     {
         ["id"] = "storagedrawers:standard_drawers_1_0",
-        ["name"] = "flesh chest",
+        ["name"] = "granite drawer",
         ["filter"] = {
 
             {
@@ -54,7 +54,7 @@ local sortFilters = {
             },
 
         },
-        ["filterType"] = "MOST",
+        ["filterType"] = "LEAST",
         ["filterExtra"] = 1,
         ["priority"] = 0
     }
@@ -62,6 +62,9 @@ local sortFilters = {
 }
 
 local inOrderOfPriority = {}
+
+local inputChestPeriph = peripheral.wrap(inputChest)
+local junkCheckPeriph = peripheral.wrap(junkChest)
 
 local function startup()
 
@@ -88,9 +91,111 @@ local function startup()
     end
 end
 
+--NOT, IS, ALL
+--ID, TAG
+-- STRICT, LEAST
 local tickNumberAt = 0
 local function tick()
     tickNumberAt = tickNumberAt + 1
+
+    print(tickNumberAt)
+
+    local listOfItems = inputChestPeriph.list()
+    if #listOfItems > 0 then
+        
+        for itemIndex, _ in pairs(listOfItems) do
+            local itemDetails = inputChestPeriph.getItemDetail(itemIndex)
+
+            print("Sorting... "..itemDetails.name)
+            local tablesToThinkAbout = {}
+            for _, tablIndex in pairs(inOrderOfPriority) do
+                local currentTable = sortFilters[tablIndex]
+                local currentTableScore = 0
+                print(tablIndex)
+                for filterIndex, filterValue in pairs(currentTable["filter"]) do
+                    local filterToCompare = filterValue["value"]
+                    local filterType = filterValue["type"]
+                    local filterArgument = filterValue["arg"]
+
+                    local passedAmt = 0
+                    if filterType == "ID" then
+                        
+                        for i,v in pairs(filterToCompare) do
+                            if v == itemDetails["name"] then
+                                passedAmt = passedAmt + 1
+                                print("Item has a similar id!")
+                            end
+                        end
+
+                    elseif filterType == "TAG" then
+
+                        for i,v in pairs(filterToCompare) do
+
+                            for i2,v2 in pairs(itemDetails["tags"]) do
+                                if v == v2 then
+                                    passedAmt = passedAmt + 1
+                                    print("Item has a similar tag!")
+                                end
+                            end
+                        end
+
+                    end
+
+                    local passed = false
+                    if filterArgument == "NOT" then
+                        if passedAmt == 0 then
+                            passed = true
+                        end
+                    elseif filterArgument == "IS" then
+                        if passedAmt > 0 then
+                            passed = true
+                        end
+                    elseif filterArgument == "ALL" then
+                        if passedAmt == #filterToCompare then
+                            passed = true
+                        end
+                    end
+
+                    if passed == true then
+                        currentTableScore = currentTableScore + 1
+                        print("Increasing score!")
+                    end
+                end
+
+                local filterType2 = sortFilters["filterType"]
+                local filterExtra = sortFilters["filterExtra"]
+                
+                local addBool = false
+                if filterType2 == "STRICT" then
+                    if currentTableScore == #currentTable["filter"] then
+                        print("STRICT Passed!")
+                        addBool = true
+                    end
+                elseif filterType2 == "LEAST" then
+                    if currentTableScore > filterExtra then
+                        print("LEAST Passed!")
+                        addBool = true
+                    end
+                end
+                if addBool == true then
+                    print("Adding!")
+
+                    table.insert(tablesToThinkAbout, {
+                        ["tableIndex"] = tablIndex,
+                        ["score"] = currentTableScore
+                    })
+                end
+            end
+
+            for i,v in pairs(tablesToThinkAbout) do
+                print(i,v)
+            end
+        end
+
+    else
+
+    end
 end
 
 startup()
+tick()
